7e3ed7ee4198245b9e041ffa8195ccd076b5a8a4
Matias Luduena <matiasluduena23@gmail.com>
2024-10-22 10:56:39 -0300

    form images context persist data

--- app/panel/muebles/nuevo/imagenes/form.tsx
+++ app/panel/muebles/nuevo/imagenes/form.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import ImageUpload from "../image-upload";
 import { useUploadThing } from "@/lib/hooks/upload";
 import { ImagesT } from "@/lib/definitions";
 import { Button } from "@/components/ui/button";
 import { LoaderCircleIcon } from "lucide-react";
+import { useFormContext } from "@/lib/context/FormContext";
 
 export default function FormImages() {
 	const [images, setImages] = useState<ImagesT[]>([]);
 	const [loading, setLoading] = useState(false);
+	const { updateDatos } = useFormContext();
 
 	const { startUpload } = useUploadThing("imageUploader", {
 		onClientUploadComplete: (res) => {
 			console.log("uploaded successfully!", res);
+			const imagePath = res.map((item) => item.url);
+			updateDatos({ images: imagePath });
+			setLoading(false);
 		},
 		onUploadError: (err) => {
 			console.error("error occurred while uploading", err);
@@ -23,15 +28,16 @@ export default function FormImages() {
 		},
 	});
 
-	const handleUpload = () => {
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 		if (images.length < 1) return;
-
+		setLoading(true);
 		const files = images.map((item) => item.file);
 
 		startUpload(files);
 	};
 	return (
-		<form action="" className="flex justify-center flex-col ">
+		<form onSubmit={handleSubmit} className="flex justify-center flex-col ">
 			<ImageUpload images={images} setImages={setImages} />
 			<Button className="mt-8 w-fit mx-auto " disabled={loading}>
 				{loading ? (
